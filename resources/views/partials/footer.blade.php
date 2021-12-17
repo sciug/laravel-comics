bb5ae43ff1b36769d610dcdb8e13849098da0b80
sciug <gaetano.sciurti@libero.it>
2021-12-17 15:28:21 +0100

    complete page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,64 @@
+<div class="background_footer">
+      <div class="container d-flex space_between_align_center footer_container">
+          <div class="footer_info d-flex">
+              <div class="info_sec">
+                  <div class="info_sec_top">
+                      <h4>DC comics</h4>
+                    <ul>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+
+                    </ul>
+
+                  </div>
+                  <div class="info_sec_bottom">
+                        <h4>DC comics</h4>
+                    <ul>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        
+
+                    </ul>
+                  </div>
+              </div>
+              <div class="info_sec">
+                    <h4>DC comics</h4>
+                    <ul>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+
+                    </ul>
+              </div>
+              <div class="info_sec">
+                    <h4>DC comics</h4>
+                    <ul>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+                        <li><a href="#">lorem</a></li>
+
+                    </ul>
+              </div>
+          </div>
+          <div class="footer_logo_png">
+              <img src="{{asset('img/dc-logo-bg.png')}}" alt="">
+          </div>
+
+      </div>
+      @include('partials.signUp')
+
+
+</div>
---
+++ resources/views/partials/signUp.blade.php
@@ -0,0 +1,15 @@
+<div class="footer_sign_up_bg">
+      <div class="container padding d-flex space_between_align_center">
+          <button class="sign-up button">SIGN-UP NOW!</button>
+          <ul class="social d-flex">
+              <li class="follow">Follow Us</li>
+              <li><a href="#"><img src="{{asset('img/footer-facebook.png')}}" alt=""></a></li>
+              <li><a href="#"><img src="{{asset('img/footer-periscope.png')}}" alt=""></a></li>
+              <li><a href="#"><img src="{{asset('img/footer-twitter.png')}}" alt=""></a></li>
+              <li><a href="#"><img src="{{asset('img/footer-youtube.png')}}" alt=""></a></li>
+               <li><a href="#"><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a></li>
+              
+          </ul>
+      </div>
+
+  </div>
